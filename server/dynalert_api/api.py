dfb9b7c2d265b3644803ac1a14727832f7889e86
Davide Quirillo <davide.quirillo@gmail.com>
2025-12-19 21:10:16 +0100

    Implemented base CRUD operations for User model and added environment variable loading.

--- server/dynalert_api/api.py
+++ server/dynalert_api/api.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+# Dynalert – a network alert manager: it receives alerts from users and makes decisions to help them
+# Copyright (C) 2025  Davide Quirillo
+# Licensed under the GNU GPL v3 or later. See LICENSE for details.
+
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
+from lib.models import User, UserBase
+from server.dynalert_api.lib.dbmgr import get_session
 
 app = FastAPI()
 
+@app.get("/api/users")
+async def get_users(db_session: Session = Depends(get_session)):
+    users = db_session.exec(select(User)).all()
+    return users
+
 @app.get("/api/user/{user_id}")
-async def get_user(user_id: str):
-    return {"user_id": user_id, "name": "John Doe"}
+async def get_user(user_id: str, db_session: Session = Depends(get_session)):
+    user = db_session.exec(select(User).where(User.id == user_id)).first()
+    return user
+
+@app.post("/api/user")
+def create_user(user: User, db_session: Session = Depends(get_session)):
+    db_session.add(user)
+    db_session.commit()
+    db_session.refresh(user)
+    return user
+
+@app.delete("/api/user/{user_id}")
+def delete_user(user_id: str, db_session: Session = Depends(get_session)):
+    user = db_session.exec(select(User).where(User.id == user_id)).first()
+    if user:
+        db_session.delete(user)
+        db_session.commit()
+        return {"message": "User deleted"}
+    return {"message": "User not found"}
+
+@app.put("/api/user/{user_id}")
+def update_user(user_id: str, user_new: UserBase, db_session: Session = Depends(get_session)):
+    user = db_session.exec(select(User).where(User.id == user_id)).first()
+    if user:
+        user.firstname = user_new.firstname
+        user.surname = user_new.surname
+        db_session.add(user)
+        db_session.commit()
+        db_session.refresh(user)
+        return user
+    return {"message": "User not found"}
---
+++ server/dynalert_api/lib/dbmgr.py
@@ -0,0 +1,19 @@
+# Dynalert – a network alert manager: it receives alerts from users and makes decisions to help them
+# Copyright (C) 2025  Davide Quirillo
+# Licensed under the GNU GPL v3 or later. See LICENSE for details.
+
+from sqlmodel import create_engine, Session
+
+engine = None
+
+# Initialize the database pool/connection
+def init(host, port, db_name, user, password):
+    global engine    
+    db_url = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+    engine = create_engine(db_url, echo=True) # echo=True for logging to console
+    return engine
+
+# Obtain a session for database operations
+def get_session():
+    with Session(engine) as session:
+        yield session
