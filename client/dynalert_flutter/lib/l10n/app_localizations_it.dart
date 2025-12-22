af9a23fb52838dedce21f153da5a84039a84de7c
Davide Quirillo <davide.quirillo@gmail.com>
2025-12-22 21:08:08 +0100

    Base internationalization support

---
+++ client/dynalert_flutter/lib/l10n/app_localizations_it.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Italian (`it`).
+class AppLocalizationsIt extends AppLocalizations {
+  AppLocalizationsIt([String locale = 'it']) : super(locale);
+
+  @override
+  String get menuHome => 'Home';
+
+  @override
+  String get menuLogin => 'Login';
+
+  @override
+  String get menuLogout => 'Logout';
+
+  @override
+  String get menuSettings => 'Impostazioni';
+
+  @override
+  String get menuTerms => 'Termini legal';
+
+  @override
+  String get menuProfile => 'Profilo';
+
+  @override
+  String get menuAdmin => 'Admin';
+}
