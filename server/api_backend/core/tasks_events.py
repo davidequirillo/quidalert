cc86ce4ff11f89157044285bed9182290ad59340
Davide Quirillo <136712898+davidequirillo@users.noreply.github.com>
2026-03-08 02:07:16 +0100

    Draft implementation of "search and notify nearby users" function

--- server/api_backend/core/tasks_events.py
+++ server/api_backend/core/tasks_events.py
@@ -6,6 +6,24 @@ from core.logging import get_tasks_logger
 
 logger = get_tasks_logger()
 
+def log_alert_error_searching_nearby_users(alert_id: str, request_info: dict):
+    logger.error(
+        f"alert_error_searching_nearby_users, alert_id={alert_id}",
+        extra=request_info
+    )
+
+def log_alert_error_searching_nearest_chief(alert_id: str, request_info: dict):
+    logger.error(
+        f"alert_error_searching_nearest_chief, alert_id={alert_id}",
+        extra=request_info
+    )
+
+def log_alert_error_saving_nearby_chief_and_users(alert_id: str, request_info: dict):
+    logger.error(
+        f"alert_error_saving_nearby_chief_and_users, alert_id={alert_id}",
+        extra=request_info
+    )
+
 def log_alert_notify_nearby_users(alert_id: str, request_info: dict):
     logger.info(
         f"alert_notify_nearby_users, alert_id={alert_id}",
