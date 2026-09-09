28891f33b87cc0418ddf00129b21b3a6c9bee593
Davide Quirillo <davide.quirillo@gmail.com>
2026-09-09 21:40:35 +0200

    Minor fixes

--- client/flutter_app/lib/services/background_location.dart
+++ client/flutter_app/lib/services/background_location.dart
@@ -460,7 +460,7 @@ class BackgroundLocationService {
         "longitude": location["coords"]?["longitude"]?.toString() ?? "n/a",
         "accuracy": location["coords"]?["accuracy"]?.toString() ?? "n/a",
         "is_moving": location["is_moving"]?.toString() ?? "n/a",
-        "activity": location["activity"]?.toString() ?? "n/a",
+        "activity": location["activity"]["type"]?.toString() ?? "n/a",
         "timestamp": locationDatetimeStr,
       });
     }
